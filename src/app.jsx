c9b8a7b58bc6c8da052db0ff503c7feb7c7d6467
Callebe Gomes <callebe.gomes@cnhind.com>
2024-01-18 13:16:10 -0600

    adding css to style the layout

--- src/app.jsx
+++ src/app.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import singleSpaReact from "single-spa-react";
 import ReactDom from "react-dom/client";
 
+import "./app.css";
+
 export default function App() {
   return (
-    <>
+    <div className="mfeAppR">
       <h1>MFE APP R</h1>
-    </>
+    </div>
   );
 }
 
